082ef74ace32dffe7a9559cb263e5e9fdad6c8c7
atrandys <37266675+atrandys@users.noreply.github.com>
2019-02-23 11:31:57 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -1,73 +1,107 @@
 #!/bin/bash
 
-cd /root
-yum install -y libtool openssl openssl-devel perl-core zlib-devel gcc wget pcre* lzo lzo-devel net-tools pam pam-devel epel-release
-sed -i "s/enabled=0/enabled=1/" /etc/yum.repos.d/epel.repo
-wget https://swupdate.openvpn.org/community/releases/openvpn-2.4.7.tar.gz
-tar xzvf openvpn-2.4.7.tar.gz
-cd openvpn-2.4.7
-./configure --prefix=/etc/openvpn
-make && make install
-yum install easy-rsa-3.0.3-1.el7
-#复制easy到openvpn
-cp -rf /usr/share/easy-rsa/ /etc/openvpn/easy-rsa
 
-#复制server.conf
-cp -f /root/openvpn-2.4.7/sample/sample-config-files/server.conf /etc/openvpn/
+rand(){
+    min=$1
+    max=$(($2-$min+1))
+    num=$(cat /dev/urandom | head -n 10 | cksum | awk -F ' ' '{print $1}')
+    echo $(($num%$max+$min))  
+}
 
-#复制vars
-cp -f /usr/share/doc/easy-rsa-3.0.3/vars.example /etc/openvpn/easy-rsa/3.0.3/vars
+install_openvpn(){
 
-cd /etc/openvpn/easy-rsa/3.0.3/
+    cd /root
+    yum install -y libtool openssl openssl-devel perl-core zlib-devel gcc wget pcre* lzo lzo-devel net-tools pam pam-devel epel-release
+    sed -i "s/enabled=0/enabled=1/" /etc/yum.repos.d/epel.repo
+    wget https://swupdate.openvpn.org/community/releases/openvpn-2.4.7.tar.gz
+    tar xzvf openvpn-2.4.7.tar.gz
+    cd openvpn-2.4.7
+    ./configure --prefix=/etc/openvpn
+    make && make install
+    yum install easy-rsa-3.0.3-1.el7
+    #复制easy到openvpn
+    cp -rf /usr/share/easy-rsa/ /etc/openvpn/easy-rsa
+    #复制server.conf
+    cp -f /root/openvpn-2.4.7/sample/sample-config-files/server.conf /etc/openvpn/
+    #复制vars
+    cp -f /usr/share/doc/easy-rsa-3.0.3/vars.example /etc/openvpn/easy-rsa/3.0.3/vars
+    cd /etc/openvpn/easy-rsa/3.0.3/
+    #生成ta.key
+    /etc/openvpn/sbin/openvpn --genkey --secret ta.key
+    #生成证书
+    ./easyrsa --batch build-ca nopass
+    #生成服务端证书
+    ./easyrsa --batch build-server-full server nopass
+    #生成客户端端证书
+    ./easyrsa --batch build-client-full client1 nopass
+    #生成gen
+    ./easyrsa gen-dh
+    mkdir /etc/openvpn/client
 
-#生成ta.key
-/etc/openvpn/sbin/openvpn --genkey --secret ta.key
-#生成证书
-./easyrsa --batch build-ca nopass
-#生成服务端证书
-./easyrsa --batch build-server-full server nopass
-#生成客户端端证书
-./easyrsa --batch build-client-full client1 nopass
-#生成gen
-./easyrsa gen-dh
+    #管理证书位置
+    cp /etc/openvpn/easy-rsa/3.0.3/pki/ca.crt /etc/openvpn/
+    cp /etc/openvpn/easy-rsa/3.0.3/pki/issued/server.crt /etc/openvpn/
+    cp /etc/openvpn/easy-rsa/3.0.3/pki/dh.pem /etc/openvpn/dh2048.pem
+    cp /etc/openvpn/easy-rsa/3.0.3/pki/private/server.key /etc/openvpn/
+    cp /etc/openvpn/easy-rsa/3.0.3/ta.key /etc/openvpn/
+    cp /etc/openvpn/easy-rsa/3.0.3/pki/issued/client1.crt /etc/openvpn/client/
+    cp /etc/openvpn/easy-rsa/3.0.3/ta.key /etc/openvpn/client/
+    cp /etc/openvpn/easy-rsa/3.0.3/pki/ca.crt /etc/openvpn/client/
+    cp /etc/openvpn/easy-rsa/3.0.3/pki/private/client1.key /etc/openvpn/client/
 
-mkdir /etc/openvpn/client
+    #关闭firewalld
+    systemctl stop firewalld
+    systemctl disable firewalld
 
-#管理证书位置
-cp /etc/openvpn/easy-rsa/3.0.3/pki/ca.crt /etc/openvpn/
-cp /etc/openvpn/easy-rsa/3.0.3/pki/issued/server.crt /etc/openvpn/
-cp /etc/openvpn/easy-rsa/3.0.3/pki/dh.pem /etc/openvpn/dh2048.pem
-cp /etc/openvpn/easy-rsa/3.0.3/pki/private/server.key /etc/openvpn/
-cp /etc/openvpn/easy-rsa/3.0.3/ta.key /etc/openvpn/
-cp /etc/openvpn/easy-rsa/3.0.3/pki/issued/client1.crt /etc/openvpn/client/
-cp /etc/openvpn/easy-rsa/3.0.3/ta.key /etc/openvpn/client/
-cp /etc/openvpn/easy-rsa/3.0.3/pki/ca.crt /etc/openvpn/client/
-cp /etc/openvpn/easy-rsa/3.0.3/pki/private/client1.key /etc/openvpn/client/
+    #安装iptables
+    yum install -y iptables-services 
+    systemctl enable iptables 
+    systemctl start iptables 
 
-#关闭firewalld
-systemctl stop firewalld
-systemctl disable firewalld
+    #清除规则
+    iptables -F
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/16 ! -d 10.8.0.0/16 -j MASQUERADE
+    service iptables save
 
-#安装iptables
-yum install -y iptables-services 
-systemctl enable iptables 
-systemctl start iptables 
+    #启用转发
+    echo 1 > /proc/sys/net/ipv4/ip_forward
 
-#清除规则
-iptables -F
-iptables -t nat -A POSTROUTING -s 10.8.0.0/16 ! -d 10.8.0.0/16 -j MASQUERADE
-service iptables save
+    #永久转发
+    echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+    sysctl -p
 
-#启用转发
-echo 1 > /proc/sys/net/ipv4/ip_forward
+    #配置服务端server.conf
+    cd /etc/openvpn
+    rm -f server.conf
+    curl -o server.conf https://raw.githubusercontent.com/atrandys/store/master/server.conf
 
-#永久转发
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+    #将openvpn客户端文件下载到client
+    curl -o /etc/openvpn/client/client.ovpn https://raw.githubusercontent.com/atrandys/store/master/client.ovpn
 
-#配置服务端server.conf
-cd /etc/openvpn
-rm -f server.conf
-curl -o server.conf https://raw.githubusercontent.com/atrandys/onekeyopenvpn/master/server.conf
+    port=$(rand 10000 60000)
+    serverip=$(curl ipv4.icanhazip.com)
+    sed -i "s/myip/$serverip/" /etc/openvpn/client/client.ovpn
+    sed -i "s/myport/$port/" /etc/openvpn/client/client.ovpn
+    sed -i "s/myport/$port/" /etc/openvpn/server.conf
 
-#将openvpn客户端文件下载到client
-curl -o /etc/openvpn/client/client.ovpn https://raw.githubusercontent.com/atrandys/onekeyopenvpn/master/client.ovpn
+    #启动openvpn
+    /etc/openvpn/sbin/openvpn --config /etc/openvpn/server.conf 
+
+#增加自启动脚本
+cat > /etc/rc.d/init.d/autoopenvpn<<-EOF
+#!/bin/sh
+#chkconfig: 2345 80 90
+#description:autoopenvpn
+/etc/openvpn/sbin/openvpn --config /etc/openvpn/server.conf 
+EOF
+
+    #设置脚本权限
+    chmod +x /etc/rc.d/init.d/autoopenvpn
+    chkconfig --add autoopenvpn
+    chkconfig autoopenvpn on
+
+}
+
+install_openvpn
+
+
